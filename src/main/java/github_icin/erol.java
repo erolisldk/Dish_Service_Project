12908923101e95c63fb008b92b77047d90cffa12
Erol <122645226+erolisldk@users.noreply.github.com>
2023-06-06 00:01:03 +0300

    main branch degisiklik

--- src/main/java/github_icin/erol.java
+++ src/main/java/github_icin/erol.java
@@ -3,6 +3,8 @@ package github_icin;
 public class erol {
     public static void main(String[] args) {
         System.out.println("erolg");
+        System.out.println("123456");
+        System.out.println("786910");
     }
 
 }

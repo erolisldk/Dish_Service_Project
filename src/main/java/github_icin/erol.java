a119fe88960a0c936a53424e0a0e24b533ff9a20
Erol <122645226+erolisldk@users.noreply.github.com>
2023-06-06 18:54:12 +0300

    12345

--- src/main/java/github_icin/erol.java
+++ src/main/java/github_icin/erol.java
@@ -7,6 +7,8 @@ public class erol {
 
         System.out.println("123456");
         System.out.println("786910");
+        // 10. satira birseyler yazdim.
+        System.out.println("erol 06.06.2023 eklemesi");
 
         System.out.println("erol 7 satirda degisiklik yapti");
 

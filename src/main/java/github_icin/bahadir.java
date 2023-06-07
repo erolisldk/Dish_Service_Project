4c7afc6f4654b911950a1ef8888e5a9888aaeaaa
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-07 17:17:05 +0300

    updated

--- src/main/java/github_icin/bahadir.java
+++ src/main/java/github_icin/bahadir.java
@@ -3,5 +3,6 @@ package github_icin;
 public class bahadir {
     public static void main(String[] args) {
         System.out.println("Herkese Merhaba");
+        System.out.println("Herkese Merhaba...");
     }
 }
